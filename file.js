c137b61add398ead960819fe89a1f6a2351062cd
Fahd Mannaa <fahdkuwait@gmail.com>
2024-03-12 16:32:21 +0300

    alex

--- file.js
+++ file.js
@@ -1,2 +1,10 @@
 var log = 'Hello World';
 console.log(log);
+// string type
+var a = "string type";
+var aa = "string type too";
+// console log the type of a
+console.log(typeof a);
+var b = 4;
+var bb = 4;
+console.log(typeof a);
--- file.ts
+++ file.ts
@@ -1,2 +1,12 @@
 const log = 'Hello World'
 console.log(log)
+
+// string type
+var a = "string type"
+var aa: string = "string type too"
+// console log the type of a
+console.log(typeof a)
+
+var b = 4
+var bb: number = 4
+console.log(typeof b)
